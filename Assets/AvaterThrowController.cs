e6903e5a53f62d5091464641331c98195451bf2a
okane <okaneeisuke@gmail.com>
2026-02-18 22:06:57 +0900

    スペースを押すと動く

--- Assets/AvaterThrowController.cs
+++ Assets/AvaterThrowController.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AvaterThrowController : MonoBehaviour
+public class AvatarThrowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Throw");
+        }
     }
 }
